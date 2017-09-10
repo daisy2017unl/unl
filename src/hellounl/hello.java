b718a10d2612034dddf63265855b8aafe196d301
daisy2017unl <zhang.li@huskers.unl.edu>
2017-09-10 18:29:03 -0500

    init

---
+++ src/hellounl/hello.java
@@ -0,0 +1,12 @@
+package hellounl;
+
+//Please print your name, partner.
+//For practice of partnership with git
+//did something
+
+
+public class hello {
+	public static void main (String[] args) {
+		System.out.println("My name is Li Zhang, Nice to meet  you, partner!");
+	}
+}
